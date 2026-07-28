9215ea16858a1e462b510269655e7c38346b1a03
085_Silva Oktaria Putri <145788056+Silvok@users.noreply.github.com>
2026-07-28 14:41:25 +0700

    chore: configure Laravel deployment for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,15 @@
 <?php
 
+$directories = [
+    '/tmp/views',
+    '/tmp/cache',
+    '/tmp/sessions',
+];
+
+foreach ($directories as $directory) {
+    if (! is_dir($directory)) {
+        mkdir($directory, 0755, true);
+    }
+}
+
 require __DIR__.'/../public/index.php';
